b285723dda94f11724e2c9df2073ed931739a45a
Darlan H <darlanhermessjb@hotmail.com>
2023-11-14 21:20:57 -0300

    add nextjs example

--- apps/web-17/src/App.tsx
+++ apps/web-17/src/App.tsx
@@ -1,30 +1,28 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 import { sum } from '@pnpm-workspaces-test/utils';
 
-function App() {
-    const [counter, setCounter] = useState(sum(1, 1))
+import logo from './logo.svg';
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React - {counter}
-        </a>
-        <button onClick={() => setCounter(counter + 1)}>Increase counter</button>
-      </header>
-    </div>
-  );
+import './App.css';
+
+function App() {
+    const [counter, setCounter] = useState(sum(1, 1));
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React - {counter}
+                </a>
+                <button onClick={() => setCounter(counter + 1)}>Increase counter</button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
